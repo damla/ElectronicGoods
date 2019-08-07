b8e597717e802700d0e357787283e44f25b63180
Damla Koksal <damlakoksal@outlook.com>
2019-08-07 16:30:51 +0300

    Created dynamic structure with category menu by using ViewComponents.

---
+++ ElectronicGoods/ElectronicGoods/Models/ProductCategoryModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElectronicGoods.Models
+{
+    public class ProductCategoryModel
+    {
+        public Product Product { get; set; }
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
---
+++ ElectronicGoods/ElectronicGoods/ViewComponents/CategoryMenuViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using ElectronicGoods.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElectronicGoods.ViewComponents
+{
+    public class CategoryMenuViewComponent: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View(CategoryRepository.Categories); 
+        }
+    }
+}
